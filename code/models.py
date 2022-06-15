b001057267319fdc273c5c9e98a736e9ff0a5e2a
mohamad mohamad <mohamad_m.2@live.com>
2022-06-15 09:40:23 +0200

    added group normalization + a functionality to fix the loaders for the test phase

--- code/models.py
+++ code/models.py
@@ -109,3 +109,58 @@ class LeNet_BatchNorm(nn.Module):
         
         
         return x
+
+
+class LeNet_GroupNorm(nn.Module):
+    def init(self,input_channel,num_group,output_size):
+        super(LeNet_GroupNorm,self).init()
+              
+        self.conv1 = nn.Conv2d(in_channels=input_channel,out_channels=6,kernel_size=(5,5),padding=0,stride=1)
+        self.GN1=nn.GroupNorm(num_group, 6)
+        #Start implementation of Layer 3 (C3) which has 16 kernels of size 5x5 with padding 0 and stride 1
+        
+        self.conv2 = nn.Conv2d(in_channels = 6, out_channels = 16,kernel_size = (5,5),padding=0,stride=1)
+        self.GN2=nn.GroupNorm(num_group, 16)
+        #Start implementation of Layer 5 (C5) which is basically flattening the data 
+            
+        self.conv3 = nn.Conv2d(in_channels = 16, out_channels = 120,kernel_size = (5,5),padding=0,stride=1)
+        self.GN3=nn.GroupNorm(num_group, 120)
+        #Start implementation of Layer 6 (F6) which has 85 Linear Neurons and input of 120
+        
+        self.L1 = nn.Linear(120,84)
+        
+        #Start implementation of Layer 7 (F7) which has 10 Linear Neurons and input of 84
+        
+        self.L2 = nn.Linear(84,output_size)
+        
+        #We have used pooling of size 2 and stride 2 in this architecture 
+        
+        self.pool = nn.AvgPool2d(kernel_size = 2, stride = 2)
+        
+        #We have used tanh as an activation function in this architecture so we will use tanh at all layers excluding F7.
+        self.act = nn.Tanh()
+        
+    #Now we will implement forward function to produce entire flow of the architecture.
+    
+    def forward(self,x):
+        x = self.GN1(self.conv1(x))
+        #We have used tanh as an activation function in this architecture so we will use tanh at all layers excluding F7.
+        x = self.act(x)
+        #Now this will be passed from pooling 
+        x = self.pool(x)
+        #Next stage is convolution
+        x = self.GN2(self.conv2(x))
+        x = self.act(x)
+        x = self.pool(x)
+        #next we will pass from conv3, here we will not pass data from pooling as per Architecture 
+        x = self.GN3(self.conv3(x))
+        x = self.act(x)
+        
+        #Now the data should be flaten and it would be passed from FC layers. 
+        x = x.view(x.size()[0], -1)
+        x = self.L1(x)
+        x = self.act(x)
+        x = self.L2(x)
+        
+        
+        return x
